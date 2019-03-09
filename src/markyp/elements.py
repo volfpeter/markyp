a2bfa2b4ded08224aee89a0c57d99b1c4837bbe2
Péter Volf <peter@milinnovations.com>
2019-03-09 21:31:34 +0100

    added the elements module and its tests

---
+++ src/markyp/elements.py
@@ -0,0 +1,248 @@
+"""
+Base `markyp` element implementations.
+"""
+
+from typing import List, Sequence, Optional
+
+from markyp import ElementType, IElement, PropertyDict, PropertyValue, is_element
+from markyp.formatters import format_element_sequence, format_properties, xml_format_element, xml_escape
+
+
+class BaseElement(IElement):
+    """
+    Base class for elements that calculate their own properties and children elements
+    only when the markup is created.
+
+    Define `__slots__` in derived classes to enjoy the performance benefits the feature provides.
+    """
+
+    __slots__ = ()
+
+    def __str__(self) -> str:
+        name = self.element_name
+        properties = self.get_element_properties()
+        properties_str = format_properties(properties) if properties is not None else ""
+        children = self.get_element_children()
+        children_str =\
+            format_element_sequence(children,  element_formatter=xml_format_element, inline=self.inline_children)\
+            if children is not None else ""
+        return f"<{name} {properties_str}>{children_str}</{name}>"
+
+    @property
+    def element_name(self) -> str:
+        """
+        The name of the element.
+
+        The default value is `self.__class__.__name__`.
+        """
+        return self.__class__.__name__
+
+    @property
+    def inline_children(self) -> bool:
+        """
+        Whether the children of the element should be placed on the same line as the element.
+
+        The default value is `False`.
+        """
+        return False
+
+    def get_element_children(self) -> Optional[Sequence[ElementType]]:
+        """
+        Returns the children elements of the element if it has children.
+        """
+        return None
+
+    def get_element_properties(self) -> Optional[PropertyDict]:
+        """
+        Returns the element properties dictionary if the element has properties.
+        """
+        return None
+
+
+class ChildrenOnlyElement(IElement):
+    """
+    Base class for elements that have no properties, only children.
+
+    Define `__slots__` in derived classes to enjoy the performance benefits the feature provides.
+    """
+
+    __slots__ = ("children",)
+
+    def __init__(self, *args: ElementType) -> None:
+        """
+        Initialization.
+
+        An arbitrary number of positional arguments are accepted that must be either `IElement`
+        instances or strings. Each positional argument will become a child of the element.
+        """
+        self.children: Sequence[ElementType] = args
+        """The child elements."""
+
+    def __str__(self) -> str:
+        name: str = self.element_name
+        return f"<{name}>{format_element_sequence(self.children, element_formatter=xml_format_element, inline=self.inline_children)}</{name}>"
+
+    @property
+    def element_name(self) -> str:
+        """
+        The name of the element.
+
+        The default value is `self.__class__.__name__`.
+        """
+        return self.__class__.__name__
+
+    @property
+    def inline_children(self) -> bool:
+        """
+        Whether the children of the element should be placed on the same line as the element.
+
+        The default value is `False`.
+        """
+        return False
+
+
+class Element(IElement):
+    """
+    Base class for elements that have both properties and children.
+
+    Child elements will be placed on new lines and string children will be XML-escaped by default.
+
+    Define `__slots__` in derived classes to enjoy the performance benefits the feature provides.
+    """
+
+    __slots__ = ("children", "properties")
+
+    def __init__(self, *args: ElementType, class_: Optional[str] = None, **kwargs: PropertyValue) -> None:
+        """
+        Initialization.
+
+        An arbitrary number of positional arguments are accepted that must be either `IElement`
+        instances or strings. Each positional argument will become a child of the element.
+
+        The `class_` keyword argument is converted into the `class` element property,
+        other keyword arguments are converted to element properties as they were defined.
+        """
+        self.children: Sequence[ElementType] = args
+        """The child elements."""
+
+        self.properties: PropertyDict = kwargs
+        """The properties to set on the element."""
+
+        if class_ is not None:
+            self.properties["class"] = class_
+
+    def __str__(self) -> str:
+        name: str = self.element_name
+        return f"<{name} {format_properties(self.properties)}>{format_element_sequence(self.children, element_formatter=xml_format_element, inline=self.inline_children)}</{name}>"
+
+    @property
+    def element_name(self) -> str:
+        """
+        The name of the element.
+
+        The default value is `self.__class__.__name__`.
+        """
+        return self.__class__.__name__
+
+    @property
+    def inline_children(self) -> bool:
+        """
+        Whether the children of the element should be placed on the same line as the element.
+
+        The default value is `False`.
+        """
+        return False
+
+
+class EmptyElement(IElement):
+    """
+    Base class for elements that have no children, only properties.
+
+    Define `__slots__` in derived classes to enjoy the performance benefits the feature provides.
+    """
+
+    __slots__ = ("properties",)
+
+    def __init__(self, class_: Optional[str] = None, **kwargs: PropertyValue) -> None:
+        """
+        Initialization.
+
+        The `class_` keyword argument is converted into the `class` element property,
+        other keyword arguments are converted to element properties as they were defined.
+        """
+        self.properties: PropertyDict = kwargs
+        """The properties to set on the element."""
+
+        if class_ is not None:
+            self.properties["class"] = class_
+
+    def __str__(self) -> str:
+        name: str = self.element_name
+        return f"<{name} {format_properties(self.properties)}></{name}>"
+
+    @property
+    def element_name(self) -> str:
+        """
+        The name of the element.
+
+        The default value is `self.__class__.__name__`.
+        """
+        return self.__class__.__name__
+
+
+class SelfClosedElement(EmptyElement):
+    """
+    Self-closed version of `EmptyElement`.
+
+    Define `__slots__` in derived classes to enjoy the performance benefits the feature provides.
+    """
+
+    __slots__ = ()
+
+    def __str__(self) -> str:
+        return f"<{self.element_name} {format_properties(self.properties)}/>"
+
+
+class StringElement(IElement):
+    """
+    Base class for elements that have a single string children and any number of properties.
+
+    Define `__slots__` in derived classes to enjoy the performance benefits the feature provides.
+    """
+
+    __slots__ = ("properties", "value")
+
+    def __init__(self, value: str, *, class_: Optional[str] = None, **kwargs: PropertyValue) -> None:
+        """
+        Initialization.
+
+        The `class_` keyword argument is converted into the `class` element property,
+        other keyword arguments are converted to element properties as they were defined.
+
+        Arguments:
+            value: The string value of the element, it will be XML-escaped automatically
+                   when the element is converted to a string.
+            class_: Optional keyword argument that is converted into the `class` element property.
+        """
+        self.value: str = value
+        """The string value of the element."""
+
+        self.properties: PropertyDict = kwargs
+        """The properties to set on the element."""
+
+        if class_ is not None:
+            self.properties["class"] = class_
+
+    def __str__(self) -> str:
+        name: str = self.element_name
+        value = xml_escape(self.value) if self.value is not None else ""
+        return f"<{name} {format_properties(self.properties)}>{value}</{name}>"
+
+    @property
+    def element_name(self) -> str:
+        """
+        The name of the element.
+
+        The default value is `self.__class__.__name__`.
+        """
+        return self.__class__.__name__
---
+++ src/test/test_elements.py
@@ -0,0 +1,210 @@
+from markyp.elements import BaseElement,\
+                            ChildrenOnlyElement,\
+                            Element,\
+                            EmptyElement,\
+                            SelfClosedElement,\
+                            StringElement
+
+def test_BaseElement():
+    class TE(BaseElement):
+        __slots__ = ("children", "properties")
+
+        def __init__(self):
+            super().__init__()
+            self.children = None
+            self.properties = None
+
+        def get_element_children(self):
+            if super().get_element_children() is not None:
+                raise Exception("BaseElement should not have children by default.")
+            return self.children
+
+        def get_element_properties(self):
+            if super().get_element_properties() is not None:
+                raise Exception("BaseElement should not have properties by default.")
+            return self.properties
+
+    te = TE()
+    assert str(te) == "<TE ></TE>"
+
+    te.children = []
+    te.properties = {}
+    assert str(te) == "<TE ></TE>"
+
+    te.children = ["First", "Second"]
+    te.properties = {}
+    assert str(te) == "<TE >\nFirst\nSecond\n</TE>"
+
+    te.children = ["First", "Second"]
+    te.properties = {"class": "test", "width": 200}
+    assert str(te) == "<TE class=\"test\" width=\"200\">\nFirst\nSecond\n</TE>"
+
+def test_ChildrenOnlyElement():
+    class TE(ChildrenOnlyElement):
+        __slots__ = ()
+
+    assert ChildrenOnlyElement().markup ==\
+        "<ChildrenOnlyElement></ChildrenOnlyElement>"
+    assert str(ChildrenOnlyElement()) ==\
+        "<ChildrenOnlyElement></ChildrenOnlyElement>"
+    assert TE().markup == "<TE></TE>"
+    assert str(TE()) == "<TE></TE>"
+
+
+    assert ChildrenOnlyElement("foo", "bar").markup ==\
+        "<ChildrenOnlyElement>\nfoo\nbar\n</ChildrenOnlyElement>"
+    assert str(ChildrenOnlyElement("foo", "bar")) ==\
+        "<ChildrenOnlyElement>\nfoo\nbar\n</ChildrenOnlyElement>"
+    assert TE("foo", "bar").markup == "<TE>\nfoo\nbar\n</TE>"
+    assert str(TE("foo", "bar")) == "<TE>\nfoo\nbar\n</TE>"
+
+    assert ChildrenOnlyElement(ChildrenOnlyElement("foo"), "bar").markup ==\
+        "<ChildrenOnlyElement>\n<ChildrenOnlyElement>\nfoo\n</ChildrenOnlyElement>\nbar\n</ChildrenOnlyElement>"
+    assert str(ChildrenOnlyElement(ChildrenOnlyElement("foo"), "bar")) ==\
+        "<ChildrenOnlyElement>\n<ChildrenOnlyElement>\nfoo\n</ChildrenOnlyElement>\nbar\n</ChildrenOnlyElement>"
+    assert TE(TE("foo"), "bar").markup == "<TE>\n<TE>\nfoo\n</TE>\nbar\n</TE>"
+    assert str(TE(TE("foo"), "bar")) == "<TE>\n<TE>\nfoo\n</TE>\nbar\n</TE>"
+
+    assert ChildrenOnlyElement("bar", ChildrenOnlyElement("foo")).markup ==\
+        "<ChildrenOnlyElement>\nbar\n<ChildrenOnlyElement>\nfoo\n</ChildrenOnlyElement>\n</ChildrenOnlyElement>"
+    assert str(ChildrenOnlyElement("bar", ChildrenOnlyElement("foo"))) ==\
+        "<ChildrenOnlyElement>\nbar\n<ChildrenOnlyElement>\nfoo\n</ChildrenOnlyElement>\n</ChildrenOnlyElement>"
+    assert TE("bar", TE("foo")).markup == "<TE>\nbar\n<TE>\nfoo\n</TE>\n</TE>"
+    assert str(TE("bar", TE("foo"))) == "<TE>\nbar\n<TE>\nfoo\n</TE>\n</TE>"
+
+    assert ChildrenOnlyElement("bar", ChildrenOnlyElement("foo"), "baz").markup ==\
+        "<ChildrenOnlyElement>\nbar\n<ChildrenOnlyElement>\nfoo\n</ChildrenOnlyElement>\nbaz\n</ChildrenOnlyElement>"
+    assert str(ChildrenOnlyElement("bar", ChildrenOnlyElement("foo"), "baz")) ==\
+        "<ChildrenOnlyElement>\nbar\n<ChildrenOnlyElement>\nfoo\n</ChildrenOnlyElement>\nbaz\n</ChildrenOnlyElement>"
+    assert TE("bar", TE("foo"), "baz").markup == "<TE>\nbar\n<TE>\nfoo\n</TE>\nbaz\n</TE>"
+    assert str(TE("bar", TE("foo"), "baz")) == "<TE>\nbar\n<TE>\nfoo\n</TE>\nbaz\n</TE>"
+
+def test_Element():
+    class TE(Element):
+        __slots__ = ()
+
+    assert Element().markup == "<Element ></Element>"
+    assert str(Element()) == "<Element ></Element>"
+    assert TE().markup == "<TE ></TE>"
+    assert str(TE()) == "<TE ></TE>"
+
+    assert Element("foo", Element(), "bar").markup == "<Element >\nfoo\n<Element ></Element>\nbar\n</Element>"
+    assert str(Element("foo", Element(), "bar")) == "<Element >\nfoo\n<Element ></Element>\nbar\n</Element>"
+    assert TE("foo", TE(), "bar").markup == "<TE >\nfoo\n<TE ></TE>\nbar\n</TE>"
+    assert str(TE("foo", TE(), "bar")) == "<TE >\nfoo\n<TE ></TE>\nbar\n</TE>"
+
+    assert Element("foo", Element(Element("baz")), "bar").markup == "<Element >\nfoo\n<Element >\n<Element >\nbaz\n</Element>\n</Element>\nbar\n</Element>"
+    assert str(Element("foo", Element(Element("baz")), "bar")) == "<Element >\nfoo\n<Element >\n<Element >\nbaz\n</Element>\n</Element>\nbar\n</Element>"
+    assert TE("foo", TE(TE("baz")), "bar").markup == "<TE >\nfoo\n<TE >\n<TE >\nbaz\n</TE>\n</TE>\nbar\n</TE>"
+    assert str(TE("foo", TE(TE("baz")), "bar")) == "<TE >\nfoo\n<TE >\n<TE >\nbaz\n</TE>\n</TE>\nbar\n</TE>"
+
+    assert Element(class_="class test", value=123).markup == "<Element value=\"123\" class=\"class test\"></Element>"
+    assert str(Element(class_="class test", value=123)) == "<Element value=\"123\" class=\"class test\"></Element>"
+    assert TE(class_="class test", value=123).markup == "<TE value=\"123\" class=\"class test\"></TE>"
+    assert str(TE(class_="class test", value=123)) == "<TE value=\"123\" class=\"class test\"></TE>"
+
+    assert Element("foo", Element(Element("baz")), "bar", class_="class test", value=4.2).markup ==\
+        "<Element value=\"4.2\" class=\"class test\">\nfoo\n<Element >\n<Element >\nbaz\n</Element>\n</Element>\nbar\n</Element>"
+    assert str(Element("foo", Element(Element("baz")), "bar", class_="class test", value=4.2)) ==\
+        "<Element value=\"4.2\" class=\"class test\">\nfoo\n<Element >\n<Element >\nbaz\n</Element>\n</Element>\nbar\n</Element>"
+    assert TE("foo", TE(TE("baz")), "bar", class_="class test", value=4.2).markup ==\
+        "<TE value=\"4.2\" class=\"class test\">\nfoo\n<TE >\n<TE >\nbaz\n</TE>\n</TE>\nbar\n</TE>"
+    assert str(TE("foo", TE(TE("baz")), "bar", class_="class test", value=4.2)) ==\
+        "<TE value=\"4.2\" class=\"class test\">\nfoo\n<TE >\n<TE >\nbaz\n</TE>\n</TE>\nbar\n</TE>"
+
+def test_EmptyElement():
+    class TE(EmptyElement):
+        __slots__ = ()
+
+    assert EmptyElement().markup == "<EmptyElement ></EmptyElement>"
+    assert str(EmptyElement()) == "<EmptyElement ></EmptyElement>"
+    assert TE().markup == "<TE ></TE>"
+    assert str(TE()) == "<TE ></TE>"
+
+    assert EmptyElement(class_="test").markup == "<EmptyElement class=\"test\"></EmptyElement>"
+    assert str(EmptyElement(class_="test")) == "<EmptyElement class=\"test\"></EmptyElement>"
+    assert TE(class_="test").markup == "<TE class=\"test\"></TE>"
+    assert str(TE(class_="test")) == "<TE class=\"test\"></TE>"
+
+    assert EmptyElement(class_="test", **{"class": "no-test"}).markup == "<EmptyElement class=\"test\"></EmptyElement>"
+    assert str(EmptyElement(class_="test", **{"class": "no-test"})) == "<EmptyElement class=\"test\"></EmptyElement>"
+    assert TE(class_="test", **{"class": "no-test"}).markup == "<TE class=\"test\"></TE>"
+    assert str(TE(class_="test", **{"class": "no-test"})) == "<TE class=\"test\"></TE>"
+
+    assert EmptyElement(string="foo", int=3, float=3.1).markup == "<EmptyElement string=\"foo\" int=\"3\" float=\"3.1\"></EmptyElement>"
+    assert str(EmptyElement(string="foo", int=3, float=3.1)) == "<EmptyElement string=\"foo\" int=\"3\" float=\"3.1\"></EmptyElement>"
+    assert TE(string="foo", int=3, float=3.1).markup == "<TE string=\"foo\" int=\"3\" float=\"3.1\"></TE>"
+    assert str(TE(string="foo", int=3, float=3.1)) == "<TE string=\"foo\" int=\"3\" float=\"3.1\"></TE>"
+
+    assert EmptyElement(class_="cls", string="foo", int=3, float=3.1).markup == "<EmptyElement string=\"foo\" int=\"3\" float=\"3.1\" class=\"cls\"></EmptyElement>"
+    assert str(EmptyElement(class_="cls", string="foo", int=3, float=3.1)) == "<EmptyElement string=\"foo\" int=\"3\" float=\"3.1\" class=\"cls\"></EmptyElement>"
+    assert TE(class_="cls", string="foo", int=3, float=3.1).markup == "<TE string=\"foo\" int=\"3\" float=\"3.1\" class=\"cls\"></TE>"
+    assert str(TE(class_="cls", string="foo", int=3, float=3.1)) == "<TE string=\"foo\" int=\"3\" float=\"3.1\" class=\"cls\"></TE>"
+
+    assert EmptyElement(**{"string": "foo", "int": 3, "float": 3.1}).markup == "<EmptyElement string=\"foo\" int=\"3\" float=\"3.1\"></EmptyElement>"
+    assert str(EmptyElement(**{"string": "foo", "int": 3, "float": 3.1})) == "<EmptyElement string=\"foo\" int=\"3\" float=\"3.1\"></EmptyElement>"
+    assert TE(**{"string": "foo", "int": 3, "float": 3.1}).markup == "<TE string=\"foo\" int=\"3\" float=\"3.1\"></TE>"
+    assert str(TE(**{"string": "foo", "int": 3, "float": 3.1})) == "<TE string=\"foo\" int=\"3\" float=\"3.1\"></TE>"
+
+def test_SelfClosedElement():
+    class TE(SelfClosedElement):
+        __slots__ = ()
+
+    assert SelfClosedElement().markup == "<SelfClosedElement />"
+    assert str(SelfClosedElement()) == "<SelfClosedElement />"
+    assert TE().markup == "<TE />"
+    assert str(TE()) == "<TE />"
+
+    assert SelfClosedElement(class_="test").markup == "<SelfClosedElement class=\"test\"/>"
+    assert str(SelfClosedElement(class_="test")) == "<SelfClosedElement class=\"test\"/>"
+    assert TE(class_="test").markup == "<TE class=\"test\"/>"
+    assert str(TE(class_="test")) == "<TE class=\"test\"/>"
+
+    assert SelfClosedElement(class_="test", **{"class": "no-test"}).markup == "<SelfClosedElement class=\"test\"/>"
+    assert str(SelfClosedElement(class_="test", **{"class": "no-test"})) == "<SelfClosedElement class=\"test\"/>"
+    assert TE(class_="test", **{"class": "no-test"}).markup == "<TE class=\"test\"/>"
+    assert str(TE(class_="test", **{"class": "no-test"})) == "<TE class=\"test\"/>"
+
+    assert SelfClosedElement(string="foo", int=3, float=3.1).markup == "<SelfClosedElement string=\"foo\" int=\"3\" float=\"3.1\"/>"
+    assert str(SelfClosedElement(string="foo", int=3, float=3.1)) == "<SelfClosedElement string=\"foo\" int=\"3\" float=\"3.1\"/>"
+    assert TE(string="foo", int=3, float=3.1).markup == "<TE string=\"foo\" int=\"3\" float=\"3.1\"/>"
+    assert str(TE(string="foo", int=3, float=3.1)) == "<TE string=\"foo\" int=\"3\" float=\"3.1\"/>"
+
+    assert SelfClosedElement(class_="cls", string="foo", int=3, float=3.1).markup == "<SelfClosedElement string=\"foo\" int=\"3\" float=\"3.1\" class=\"cls\"/>"
+    assert str(SelfClosedElement(class_="cls", string="foo", int=3, float=3.1)) == "<SelfClosedElement string=\"foo\" int=\"3\" float=\"3.1\" class=\"cls\"/>"
+    assert TE(class_="cls", string="foo", int=3, float=3.1).markup == "<TE string=\"foo\" int=\"3\" float=\"3.1\" class=\"cls\"/>"
+    assert str(TE(class_="cls", string="foo", int=3, float=3.1)) == "<TE string=\"foo\" int=\"3\" float=\"3.1\" class=\"cls\"/>"
+
+    assert SelfClosedElement(**{"string": "foo", "int": 3, "float": 3.1}).markup == "<SelfClosedElement string=\"foo\" int=\"3\" float=\"3.1\"/>"
+    assert str(SelfClosedElement(**{"string": "foo", "int": 3, "float": 3.1})) == "<SelfClosedElement string=\"foo\" int=\"3\" float=\"3.1\"/>"
+    assert TE(**{"string": "foo", "int": 3, "float": 3.1}).markup == "<TE string=\"foo\" int=\"3\" float=\"3.1\"/>"
+    assert str(TE(**{"string": "foo", "int": 3, "float": 3.1})) == "<TE string=\"foo\" int=\"3\" float=\"3.1\"/>"
+
+def test_StringElement():
+    class TE(StringElement):
+        __slots__ = ()
+
+    assert StringElement("").markup == "<StringElement ></StringElement>"
+    assert str(StringElement("")) == "<StringElement ></StringElement>"
+    assert TE("").markup == "<TE ></TE>"
+    assert str(TE("")) == "<TE ></TE>"
+
+    assert StringElement(None).markup == "<StringElement ></StringElement>"
+    assert str(StringElement(None)) == "<StringElement ></StringElement>"
+    assert TE(None).markup == "<TE ></TE>"
+    assert str(TE(None)) == "<TE ></TE>"
+
+    assert StringElement("value").markup == "<StringElement >value</StringElement>"
+    assert str(StringElement("value")) == "<StringElement >value</StringElement>"
+    assert TE("value").markup == "<TE >value</TE>"
+    assert str(TE("value")) == "<TE >value</TE>"
+
+    assert StringElement("value", class_="cls test").markup == "<StringElement class=\"cls test\">value</StringElement>"
+    assert str(StringElement("value", class_="cls test")) == "<StringElement class=\"cls test\">value</StringElement>"
+    assert TE("value", class_="cls test").markup == "<TE class=\"cls test\">value</TE>"
+    assert str(TE("value", class_="cls test")) == "<TE class=\"cls test\">value</TE>"
+
+    assert StringElement("value", class_="cls", foo=42, bar="bar").markup == "<StringElement foo=\"42\" bar=\"bar\" class=\"cls\">value</StringElement>"
+    assert str(StringElement("value", class_="cls", foo=42, bar="bar")) == "<StringElement foo=\"42\" bar=\"bar\" class=\"cls\">value</StringElement>"
+    assert TE("value", class_="cls", foo=42, bar="bar").markup == "<TE foo=\"42\" bar=\"bar\" class=\"cls\">value</TE>"
+    assert str(TE("value", class_="cls", foo=42, bar="bar")) == "<TE foo=\"42\" bar=\"bar\" class=\"cls\">value</TE>"
