8b679f33389f2661d0f78593466a7666b8058c3f
Péter Volf <peter@milinnovations.com>
2019-03-09 21:31:09 +0100

    project initialization

---
+++ src/markyp/__init__.py
@@ -0,0 +1,50 @@
+"""
+Type declarations and the most basic building blocks of `markyp`.
+"""
+
+from typing import Any, Dict, TypeVar, Union
+
+
+class IElement(object):
+    """
+    Abstract base class for markup elements.
+    """
+
+    __slots__ = ()
+
+    def __str__(self) -> str:
+        """
+        Returns the string representation of the element including all its children.
+        """
+        raise NotImplementedError("IElement is abstract, please override __str__() in the child class.")
+
+    @property
+    def markup(self) -> str:
+        """
+        The string representation of the element including all its children.
+
+        This property is just a proxy for `__str__()`.
+        """
+        return str(self)
+
+
+ElementType = Union[IElement, str]
+"""Type denoting `IElement` or string objects."""
+
+
+PropertyValue = Union[str, int, float, bool]
+"""Type to use for type hinting element properties."""
+
+
+PropertyDict = Dict[str, PropertyValue]
+"""Type representing property name - property value mapping."""
+
+
+def is_element(item: Any) -> bool:
+    """
+    Returns whether the received item is an `ElementType`.
+
+    Arguments:
+        item: The item to check.
+    """
+    return isinstance(item, (IElement, str))
