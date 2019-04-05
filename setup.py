4e2baab8adf0eb02d09539b11bae52f1161a0ec2
Péter Volf <peter@milinnovations.com>
2019-04-05 09:14:32 +0200

    load install_requires from requirements.txt

--- setup.py
+++ setup.py
@@ -7,9 +7,22 @@ from setuptools import setup, find_packages
 with open(path.join(path.abspath(path.dirname(__file__)), "README.md"), encoding="utf-8") as f:
     readme = f.read()
 
+# Get the version from the root __init__.py file.
 with open(path.join(path.abspath(path.dirname(__file__)), "src", "markyp", "__init__.py"), encoding="utf-8") as f:
     match = re.search("__version__ = \"(.*?)\"", f.read())
     version = match.group(1) if match else "0.0.0"
+
+# Get the requirements from requirements.txt.
+req_filename = "requirements.txt"
+exp = re.compile("(?P<req>\\w+)\\s*(?P<op>[<>=!~]+)\\s*(?P<ver>[\\w.]+)")
+requirements = []
+with open(path.join(path.dirname(path.abspath(__file__)), req_filename)) as req_file:
+    for line in req_file:
+        line = line.split("#", maxsplit=1)[0].strip()
+        match = exp.match(line) if line else None
+        if match is not None:
+            requirements.append(("".join((match["req"], match["op"], match["ver"]))))
+requirements.sort(key=lambda s: s.casefold())
 
 setup(
     name="markyp",
@@ -42,5 +55,5 @@ setup(
     package_dir={"": "src"},
     packages=find_packages("src", exclude=["test"]),
     python_requires=">=3.6",
-    install_requires=[]
+    install_requires=requirements
 )
