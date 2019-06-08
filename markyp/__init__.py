9cbc3d62db836b62ffb03de5859fb8ae2835087d
Péter Volf <peter@milinnovations.com>
2019-06-08 09:36:04 +0200

    bumped the version number

--- markyp/__init__.py
+++ markyp/__init__.py
@@ -9,7 +9,7 @@ __copyright__ = "Copyright 2019, Peter Volf"
 __email__ = "do.volfp@gmail.com"
 __license__ = "MIT"
 __url__ = "https://github.com/volfpeter/markyp"
-__version__ = "0.1905.1"
+__version__ = "0.1906.0"
 
 
 __all__ = ("IElement", "ElementType", "PropertyValue", "PropertyDict", "is_element")
