ffe22d0f31c46d588d0494edb7b0c803648afa7f
Péter Volf <peter@milinnovations.com>
2019-09-12 23:22:34 +0200

    added more tests

--- test/test_parser.py
+++ test/test_parser.py
@@ -1,4 +1,4 @@
-from markyp import ElementType
+from markyp import ElementType, PropertyDict
 from markyp.elements import (
     ChildrenOnlyElement,
     Element,
@@ -6,25 +6,53 @@ from markyp.elements import (
     SelfClosedElement,
     StringElement,
 )
-from markyp.parser import Parser, ParserRule
+from markyp.parser import AnyElement, Converter, Parser, ParserRule
 
 
 def test_parser():
-    element = get_root_element()
-    generated = element.markup
+    element = get_elements()
+    markup = element.markup
 
     for i, parser in enumerate(get_parsers()):
-        parsed = parser.fromstring(generated)
+        parsed = parser.fromstring(markup)
         assert_elements_equal(parsed, element)
-        assert parsed.markup == generated
+        assert parsed.markup == markup
 
 
-def get_root_element():
+def test_converter():
+    element = get_elements()
+    markup = element.markup
+    converted_element = get_converted_elements()
+    converted_markup = converted_element.markup
+
+    for i, parser in enumerate(get_parsers(converter)):
+        parsed = parser.fromstring(markup)
+        assert_elements_equal(parsed, converted_element)
+        assert parsed.markup == converted_markup
+
+
+def get_elements():
     return ChildrenOnlyElement(
         Element("Children", attr1="1"),
         EmptyElement(attr1="11", attr2="22"),
-        SelfClosedElement(attr1="111", attr2="222", class_="self-closed"),
+        SelfClosedElement(attr1="111", attr2="222"),
         StringElement("String children"),
+        AnyElement("AnyElement content", element_tag="Any", attr="any-element"),
+    )
+
+
+def get_converted_elements():
+    return ChildrenOnlyElement(
+        Element("Children", attr1="1", converter="applied"),
+        EmptyElement(attr1="11", attr2="22", converter="applied"),
+        SelfClosedElement(attr1="111", attr2="222", converter="applied"),
+        StringElement("String children", converter="applied"),
+        AnyElement(
+            "AnyElement content",
+            element_tag="Any",
+            attr="any-element",
+            converter="applied",
+        ),
     )
 
 
@@ -44,7 +72,7 @@ def assert_elements_equal(foo: ElementType, bar: ElementType):
             assert_elements_equal(foo.children[i], bar.children[i])  # type: ignore
 
 
-def get_parsers():
+def get_parsers(converter=None):
     parser_1 = Parser(
         ChildrenOnlyElement, Element, EmptyElement, SelfClosedElement, StringElement
     )
@@ -70,8 +98,21 @@ def get_parsers():
         ParserRule("SelfClosedElement", SelfClosedElement),
         ParserRule("StringElement", StringElement),
     )
-    return (parser_1, parser_2, parser_3, parser_4, parser_5)
+
+    parsers = (parser_1, parser_2, parser_3, parser_4, parser_5)
+    for p in parsers:
+        p.converter(converter)
+
+    return parsers
 
 
 class ErrorElement(Element):
     __slots__ = ()
+
+
+def converter(factory, children, properties):
+    if factory == ChildrenOnlyElement:
+        return factory, children, properties
+
+    properties["converter"] = "applied"
+    return factory, children, properties
